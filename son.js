d1a90cb90dd9e89f9a0f81860e228828622d4c71
Willy-won <64010298+Willy-won@users.noreply.github.com>
2020-08-20 13:35:47 +0300

    Delete son.js

--- son.js
+++
@@ -1,5 +0,0 @@
-jQuery( document ).ready(function() { 
-  jQuery('.love').click(function(){
-    jQuery(this).html('<p class="final">Joyeux anniversaire mon poulet&nbsp;!</p><img src="http://i.giphy.com/26FLdmIp6wJr91JAI.gif" width="480" height="270" alt="Love you man">');
-  });
- });
