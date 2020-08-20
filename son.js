85f97d0deb1e9aa2cc54cdeb9b791115e8ae14bd
Willy-won <64010298+Willy-won@users.noreply.github.com>
2020-08-20 13:29:36 +0300

    Add files via upload

---
+++ son.js
@@ -0,0 +1,5 @@
+jQuery( document ).ready(function() { 
+  jQuery('.love').click(function(){
+    jQuery(this).html('<p class="final">Joyeux anniversaire mon poulet&nbsp;!</p><img src="http://i.giphy.com/26FLdmIp6wJr91JAI.gif" width="480" height="270" alt="Love you man">');
+  });
+ });
